bab6f8a2e951d40b3d529b3179bf91c8072e53a8
SWilson333 <43420037+SWilson333@users.noreply.github.com>
2021-11-04 16:18:30 -0400

    That new new

--- FlaskPetStore/FlaskPetStore/FlaskPetStore.py
+++ FlaskPetStore/FlaskPetStore/FlaskPetStore.py
@@ -3,17 +3,18 @@ from flask import Flask, render_template,flash, redirect
 from forms import NewClothingForm
 
 class clothing:
-	def __init__(self,brand,type,price,pic,size):
+	def __init__(self,id,brand,type,price,pic,size):
+		self.id = id
 		self.brand = brand
 		self.type = type
 		self.price = price
 		self.pic = pic
 		self.size = size
 clothinglist = []
-c = clothing("Gucci","Shirt",200.00,"GucciShirt.jpg","Large")
-c2 = clothing("Nike","Shorts",55.00, "NikeShorts.jpg","Medium")
-c3 = clothing("Wrangler","Jeans",20.00, "WranglerJeans.jpg","Small")
-c4 = clothing("North Face","Hoodie",90.00, "NorthfaceHoodie.jpg","Medium")
+c = clothing(0,"Gucci","Shirt",200.00,"GucciShirt.jpg","Large")
+c2 = clothing(1,"Nike","Shorts",55.00, "NikeShorts.jpg","Medium")
+c3 = clothing(2,"Wrangler","Jeans",20.00, "WranglerJeans.jpg","Small")
+c4 = clothing(3,"North Face","Hoodie",90.00, "NorthfaceHoodie.jpg","Medium")
 
 clothinglist.append(c)
 clothinglist.append(c2)
@@ -28,9 +29,9 @@ def addclothing():
 	addform = NewClothingForm()
 	if addform.validate_on_submit():
 		flash('Adding clothing {}, which is a...{}'.format(
-			addform.clothingname.data, addform.clothingtype.data))
+			addform.clothingbrand.data, addform.clothingtype.data))
 		nextclothingid = len(clothinglist)
-		newclothing = clothing(nextclothingid,addform.clothingname.data,addform.clothingtype.data,addform.clothingprice.data,addform.clothingpic.data)
+		newclothing = clothing(nextclothingid,addform.clothingbrand.data,addform.clothingtype.data,addform.clothingprice.data,addform.clothingpic.data)
 		clothinglist.append(newclothing)
 		return redirect('/')
 	return render_template("addclothing.html",title='Add A Piece Of Clothing!',form=addform)
